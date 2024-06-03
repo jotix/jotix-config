5f681df63ff182d0d4ee6d1a72d18e578562931b
jotix <jujodeve@gmail.com>
2024-06-03 08:50:14 -0300

    hypr

--- modules/default.nix
+++ modules/default.nix
@@ -2,7 +2,11 @@
 
 { pkgs, ... }:
 
-{  
+{
+  imports = [
+    ./hyprland.nix
+  ];
+  
   nix = {
     package = pkgs.nixFlakes; # or versioned attributes like nixVersions.nix_2_8
     extraOptions = ''experimental-features = nix-command flakes'';
