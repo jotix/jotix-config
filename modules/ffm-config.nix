f37b0e49f6e2d14b4d1f693c798baa12a9407682
jotix <jujodeve@gmail.com>
2024-06-08 10:12:53 -0300

    filofem user

--- modules/ffm-config.nix
+++ modules/ffm-config.nix
@@ -10,7 +10,13 @@
   };
 
   environment.shellAliases.camara = "mpv rtsp://jujodeve:SuperJoti3275@192.168.0.6/stream1 --profile=low-latency --no-audio";
-
+  
+  users.users.filofem = {
+    isNormalUser = true;
+    description = "FILOfem";
+    extraGroups = [ "networkmanager" "libvirtd" ];
+  };
+  
   boot.loader.grub = {
     useOSProber = true;
     default = "2";
