d5176335e10ad5c5fde0b70142d6efa63d33c09b
jotix <jujodeve@gmail.com>
2024-06-25 18:06:04 -0300

    txx-price-update

--- nixos-modules/tcc-price-update.nix
+++ nixos-modules/tcc-price-update.nix
@@ -1,18 +1,19 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
-{
-  pkgs.writeShellApplication {
-    
-    name = "tcc-prices-update";
-    
+let
+  tcc-price-update = pkgs.writeShellApplication {
+    name = "tcc-price-update";
+
     runtimeInputs = [ pkgs.curl pkgs.unzip pkgs.rsync ];
-    
+
     text = ''
-curl -o ~/Downloads/PriceTable.zip 'https://us.tamrieltradecentre.com/download/PriceTable'
-unzip -o ~/Downloads/PriceTable.zip -d ~/Downloads/PriceTable
-cd ~/Downloads/PriceTable
-rsync -auvzhPX --progress ~/Downloads/PriceTable/. "/home/jotix/Elder Scrolls Online/live/AddOns/TamrielTradeCentre"
-'';
-    
+      curl -o ~/Downloads/PriceTable.zip 'https://us.tamrieltradecentre.com/download/PriceTable'
+      unzip -o ~/Downloads/PriceTable.zip -d ~/Downloads/PriceTable
+      cd ~/Downloads/PriceTable
+      rsync -auvzhPX --progress ~/Downloads/PriceTable/. "/home/jotix/Elder Scrolls Online/live/AddOns/TamrielTradeCentre"
+    '';
   };
+in
+{
+  environment.systemPackages = [ tcc-price-update ];
 }
