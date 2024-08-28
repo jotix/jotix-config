84612d3a62fa5ab241b88c2b84433d890654600a
jotix <jujodeve@gmail.com>
2024-08-28 19:06:51 -0300

    ttc-price-update

--- modules/ttc-price-update.nix
+++ modules/ttc-price-update.nix
@@ -1,19 +1,39 @@
 { pkgs, ... }:
 
 let
-  tcc-price-update = pkgs.writeShellApplication {
-    name = "tcc-price-update";
 
-    runtimeInputs = [ pkgs.curl pkgs.unzip pkgs.rsync ];
+script = ''
+#!/usr/bin/env bash
 
-    text = ''
-      curl -o ~/Downloads/PriceTable.zip 'https://us.tamrieltradecentre.com/download/PriceTable'
-      unzip -o ~/Downloads/PriceTable.zip -d ~/Downloads/PriceTable
-      cd ~/Downloads/PriceTable
-      rsync -auvzhPX --progress ~/Downloads/PriceTable/. "/home/jotix/Elder Scrolls Online/live/AddOns/TamrielTradeCentre"
-    '';
-  };
+TTC_PATH="/steamapps/compatdata/306130/pfx/drive_c/users/steamuser/Documents/Elder Scrolls Online/live/AddOns/TamrielTradeCentre"
+
+JTX_STEAM_PATH="/mnt/jtx-ssd/SteamLibrary"
+
+FFM_STEAM_PATH="/home/jotix/.local/share/Steam"
+
+if [[ "$HOSTNAME" == "jtx-nixos" ]]; then
+    TTC_PATH=$JTX_STEAM_PATH$TTC_PATH
+else
+    TTC_PATH=$FFM_STEAM_PATH$TTC_PATH
+fi
+
+curl -o ~/Downloads/PriceTable.zip 'https://us.tamrieltradecentre.com/download/PriceTable'
+unzip -o ~/Downloads/PriceTable.zip -d ~/Downloads/PriceTable
+cd ~/Downloads/PriceTable
+
+rsync -auvzhPX --progress ~/Downloads/PriceTable/. "$TTC_PATH"
+'';
+
+ttc-price-update = pkgs.writeShellApplication {
+  name = "ttc-price-update";
+
+  runtimeInputs = [ pkgs.curl pkgs.unzip pkgs.rsync ];
+
+  text = script;
+};
+
 in
+
 {
-  environment.systemPackages = [ tcc-price-update ];
+  environment.systemPackages = [ ttc-price-update ];
 }
