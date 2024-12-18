6400d3cc725b7c65e39a317f3b537483139cfd1d
jotix <jujodeve@gmail.com>
2024-12-17 22:05:31 -0300

    xprinter

--- modules/cups/cups-xprinterpos.nix
+++ modules/cups/cups-xprinterpos.nix
@@ -26,7 +26,7 @@ stdenv.mkDerivation {
   installPhase = ''
     install -d -m 777 $out/share/cups/model/xprinterpos/
     install -m 644 ppd/*.ppd $out/share/cups/model/xprinterpos/
-    install -m 755 -D filter/* $out/lib/cups/filter/
+    install -m 755 -D filter/x64/rasterosnailep-pos $out/lib/cups/filter/rasterosnailep-pos
   '';
 
   meta = with lib; {
