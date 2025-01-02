296616a63e58af690a747498b305d621ae19b0df
jotix <jujodeve@gmail.com>
2025-01-02 10:26:09 -0300

    Create zed.nix

---
+++ home-manager/home-manager-modules/zed.nix
@@ -0,0 +1,32 @@
+### zed Module
+
+{ config, lib, pkgs, ... }:
+
+{
+  options.OPTION.enable = lib.mkEnableOption "Enable zed editor";
+  
+  config = lib.mkIf(config.zed.enable) {
+
+    programs.zed-editor = {
+      enable = true;
+      extensions = [
+        "nix"
+      ];
+      userSettings = {
+          ui_font_size = 16;
+          buffer_font_size = 14;
+          theme = {
+            mode = "system";
+            light = "One Light";
+            dark = "One Dark";
+          };
+          buffer_font_family = "Source Code Pro";
+          terminal = {
+            font_family = "Source Code Pro";
+            line_height = "standard";
+          };
+        };
+    };
+
+  };
+}
