36cbaf91e5bcb9831f68c79db44179244702f42f
jotix <jujodeve@gmail.com>
2025-01-02 13:34:52 -0300

    tabulation

--- flake.nix
+++ flake.nix
@@ -9,37 +9,37 @@
   };
 
   outputs = { nixpkgs, home-manager, ... }:
-    let hm-config = {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.backupFileExtension = "bak";
-        home-manager.users.jotix = import ./home-manager/home.nix;
-    };
-    in
-    {
-        nixosConfigurations = {
-        jtx-nixos = nixpkgs.lib.nixosSystem {
+  let hm-config = {
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+    home-manager.backupFileExtension = "bak";
+    home-manager.users.jotix = import ./home-manager/home.nix;
+  };
+  in
+  {
+    nixosConfigurations = {
+      jtx-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-            modules = [
-            ./hosts/jtx-config.nix
-            ./config.nix
-            ./hardware-config.nix
-            home-manager.nixosModules.home-manager
-            hm-config
-            ];
-        };
+        modules = [
+          ./hosts/jtx-config.nix
+          ./config.nix
+          ./hardware-config.nix
+          home-manager.nixosModules.home-manager
+          hm-config
+        ];
+      };
 
-        ffm-nixos = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-            ./hosts/ffm-config.nix
-            ./config.nix
-            ./hardware-config.nix
-            home-manager.nixosModules.home-manager
-            hm-config
-            ];
-        };
-        };
+      ffm-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/ffm-config.nix
+          ./config.nix
+          ./hardware-config.nix
+          home-manager.nixosModules.home-manager
+          hm-config
+        ];
+      };
     };
+  };
 
 }
