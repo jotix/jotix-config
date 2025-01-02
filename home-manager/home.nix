3e59f12a634855d63e6c1e739f7bc07d618e0469
jotix <jujodeve@gmail.com>
2025-01-02 10:27:48 -0300

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -43,27 +43,6 @@
       };
     };
 
-    programs.zed-editor = {
-      enable = true;
-      extensions = [
-        "nix"
-      ];
-      userSettings = {
-          ui_font_size = 16;
-          buffer_font_size = 14;
-          theme = {
-            mode = "system";
-            light = "One Light";
-            dark = "One Dark";
-          };
-          buffer_font_family = "Source Code Pro";
-          terminal = {
-            font_family = "Source Code Pro";
-            line_height = "standard";
-          };
-        };
-    };
-
     programs.lazygit.enable = true;
 
     programs.powerline-go.enable = true;
