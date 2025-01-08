28b14aa53aa7c400c00d23f9f11c2915726cd542
jotix <jujodeve@gmail.com>
2025-01-08 00:53:57 -0300

    plasma default

--- home-manager/home-manager-modules/default.nix
+++ home-manager/home-manager-modules/default.nix
@@ -17,8 +17,6 @@
   emacs.enable = lib.mkDefault true;
   openttd.enable = lib.mkDefault true;
   # neovim.enable = lib.mkDefault true;
-  dconf-settings.enable = lib.mkDefault true;
-  ghostty.enable = true;
   zed.enable = lib.mkDefault true;
 
 }
--- modules/gnome.nix
+++ modules/gnome.nix
@@ -12,6 +12,9 @@
 
   config = lib.mkIf (config.gnome.enable) {
 
+    home-manager.users.jotix.ghostty.enable = lib.mkDefault true;
+    home-manager.users.jotix.dconf-settings.enable = lib.mkDefault true;
+
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
