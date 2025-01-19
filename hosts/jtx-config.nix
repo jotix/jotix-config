da8cc45739edb3039f4b400cc1cbddb4ab389c65
jotix <jujodeve@gmail.com>
2025-01-18 23:51:58 -0300

    refactor

--- hosts/jtx-config.nix
+++ hosts/jtx-config.nix
@@ -1,21 +1,16 @@
 # jtx-nixos hosts configuration
 
 { ... }:
-let
-  f1tv_script = ''
-    #!/usr/bin/env bash
-    export $(dbus-launch); kwin_wayland "firefox -P" &
-  '';
-in
+
 {
   ### options
   qmk.enable = true;
+  plasma.enable = false;
+  gnome.enable = true;
 
   networking = {
     hostName = "jtx-nixos";
   };
-
-  environment.shellAliases.f1tv = f1tv_script;
 
   ### jtx-nvme
   fileSystems."/mnt/jtx-nvme" = {
@@ -26,11 +21,6 @@ in
 
   ### bluetooth
   hardware.bluetooth.enable = true;
-
-  ### sddm refresh rate
-  services.xserver.displayManager.setupCommands = ''
-    kscreen-doctor output.HDMI-A-1.position.0,0 output.HDMI-A-1.mode.3840x2160@60
-  '';
 
   boot.kernelParams = [
     #"video=DP-1:1920x1080@60"
